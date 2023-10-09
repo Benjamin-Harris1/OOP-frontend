f4c678003a052a6a0382b7a55f89649e1e03f3a9
Benjamin-Harris1 <beha1503@gmail.com>
2023-10-09 11:01:16 +0200

    model

--- model/album.js
+++ model/album.js
@@ -2,5 +2,9 @@ export default class Album {
   constructor(obj) {
     this.title = obj.title;
     this.release_date = obj.release_date;
+    this.tracks = [];
+  }
+  addTrack(track) {
+    this.tracks.push(track);
   }
 }
--- model/track.js
+++ model/track.js
@@ -2,5 +2,9 @@ export default class Track {
   constructor(obj) {
     this.title = obj.title;
     this.duration = obj.duration;
+    this.artists = [];
+  }
+  addArtist(artist) {
+    this.artists.push(artist);
   }
 }
