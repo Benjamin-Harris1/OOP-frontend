804b371cb3c24617f346aa33c6717b38d3f29319
Benjamin-Harris1 <beha1503@gmail.com>
2023-10-10 18:38:22 +0200

    date

--- view/albumrenderer.js
+++ view/albumrenderer.js
@@ -3,15 +3,16 @@ import ItemRenderer from "./itemrenderer.js";
 export default class AlbumRenderer extends ItemRenderer {
   render(album) {
     const releaseDate = new Date(album.release_date);
-    const formattedDate = `${releaseDate.getDate().toString().padStart(2, "0")}-${(releaseDate.getMonth() + 1)
-      .toString()
-      .padStart(2, "0")}-${releaseDate.getFullYear()}`;
-    const html =
-      /*html*/
-      `
+    const formattedDate = releaseDate.toLocaleDateString("da-DK", {
+      day: "numeric",
+      month: "numeric",
+      year: "numeric",
+    });
+
+    const html = `
       <div class="grid-item">
-      <h2>${album.title}</h2>
-      <p>Release date: ${formattedDate}</p>
+        <h2>${album.title}</h2>
+        <p>Release date: ${formattedDate}</p>
       </div>
     `;
     return html;
