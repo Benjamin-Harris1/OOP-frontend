c7f878edb43d2cbc5713de03568e2506c5511365
Benjamin-Harris1 <beha1503@gmail.com>
2023-10-12 15:24:57 +0200

    dialog

---
+++ view/artistdialog/artistcreatedialog.js
@@ -0,0 +1,22 @@
+import Dialog from "../dialog.js";
+import Artist from "../../model/artist.js";
+import * as controller from "../app.js";
+
+export default class ArtistCreateDialog extends Dialog {
+  renderHTML() {
+    const html =
+      /*html*/
+      `
+      <h1>Create artist</h1>
+      <form action="" method="dialog" id="create-form">
+      <label for="create-name"></label>
+      <input type="text" id="create-name" name="name" placeholder="Type the name of the artist here ...">
+      <label for="create-career_start" name="career_start" placeholder="Type the date of the artists career start"></label>
+      <button data-action="create">Create</button>
+      </form>
+    `;
+    return html;
+  }
+
+  create() {}
+}
--- view/dialog.js
+++ view/dialog.js
@@ -1,1 +1,19 @@
-export default class Dialog {}
+export default class Dialog {
+  constructor(id) {
+    this.dialog = document.createElement("dialog");
+    this.dialog.id = id;
+    document.querySelector("initApp").insertAdjacentElement("afterend", this.dialog);
+  }
+
+  close() {
+    this.dialog.close();
+  }
+
+  show() {
+    this.dialog.showModal();
+  }
+
+  render() {
+    const html = this.renderHTML()
+  }
+}
