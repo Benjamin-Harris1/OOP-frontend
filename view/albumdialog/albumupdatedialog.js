3bb6c2c0b7a491f59b063a4d4018914a6e035143
Benjamin-Harris1 <beha1503@gmail.com>
2023-10-23 09:52:30 +0200

    trying to fix github pages.

--- view/albumdialog/albumupdatedialog.js
+++ view/albumdialog/albumupdatedialog.js
@@ -1,5 +1,5 @@
 import Album from "../../model/album.js";
-import * as controller from "../../../app.js";
+import * as controller from "../../app.js";
 import Dialog from "../dialog.js";
 import * as REST from "../../rest.js";
 
